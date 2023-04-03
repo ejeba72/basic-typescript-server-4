34e3f49753e5c2c93602aa51f113c681970cfd46
Emmanuel Eni <eejeba@gmail.com>
2023-04-03 14:43:03 +0100

    basic server set up

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("take a look at me now!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+    try {
+        console.log("get request");
+        res.status(200).send("hello, world");
+    }
+    catch (err) {
+        console.log(err.message);
+        res.status(500).send(err);
+    }
+});
+app.listen(PORT, () => {
+    console.log(`Server is attentively listening for requests @ port ${PORT}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("take a look at me now!");
+import express, { Express, Request, Response } from "express";
+import { config } from "dotenv";
+
+config();
+
+const app: Express = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  try {
+    console.log("get request");
+    res.status(200).send("hello, world");
+  } catch (err: any) {
+   console.log(err.message);
+   res.status(500).send(err); 
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is attentively listening for requests @ port ${PORT}`);
+});
