372981fe4e8e774c2d32d926468493000fa2f4d4
Emmanuel Eni <eejeba@gmail.com>
2023-04-09 15:22:50 +0100

    added user and address interfaces to the user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,16 +1,30 @@
-import { Schema } from "mongoose";
+import { Schema, Document, model } from "mongoose";
 
 function validateEmail(email: string) {
   const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return regex.test(email);
 }
-
 function validatePhoneNo(phoneNo: string) {
   const regex = /^(0|\+\d{1,3}0?)\d{10}$/;
   return regex.test(phoneNo);
 }
-
-const addressSchema = new Schema({
+interface IAddress {
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+}
+interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  phoneNo: string;
+  sex: "male" | "female" | "other";
+  age: number;
+  address: IAddress;
+}
+const addressSchema = new Schema<IAddress>({
   street: {
     type: String,
     required: true,
@@ -32,8 +46,7 @@ const addressSchema = new Schema({
     max: [ 100, "Sorry, we can only afford a maximum of 100 characters for your country's name" ]
   },
 })
-
-const userSchema = new Schema(
+const userSchema = new Schema<IUser>(
   {
     firstName: {
       type: String,
@@ -49,6 +62,9 @@ const userSchema = new Schema(
       type: String,
       required: true,
       lowercase: true,
+      trim: true,
+      unique: true,
+      validate: [validateEmail, "Please enter a valid email."],
       max: [ 200, "Sorry, we can only afford a maximum of 200 characters for your email address."]
     },
     password: {
@@ -56,6 +72,10 @@ const userSchema = new Schema(
       required: true,
       min: [ 8, "Heads up! Your password is too short. It must have at least 8 characters." ],
       max: [ 500, "Wow, your effort to have a secured password is very impressive! Alas, we can only afford a maximum of 500 characters for your password."]
+    },
+    phoneNo: {
+      type: String,
+      validate: [validatePhoneNo, "Please enter a valid phone number"]
     },
     sex: {
       type: String,
@@ -72,3 +92,6 @@ const userSchema = new Schema(
   },
   {timestamps: true}
 )
+const userModel = model("user", userSchema);
+
+export default { userModel }
